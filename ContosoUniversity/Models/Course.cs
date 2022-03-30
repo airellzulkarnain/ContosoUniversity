6b096019a9c0fe536c7fa07ef5f17cbbcaa89406
airellzulkarnain <airell.zulkarnain@gmail.com>
2022-03-30 16:29:34 +0700

    Get Started Update The Database Context Class

---
+++ ContosoUniversity/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
